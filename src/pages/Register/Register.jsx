7f093d688717a357c63edd42e9a1104b1db598ab
bmbx08 <bmbx08@bu.ac.kr>
2025-05-07 16:56:57 +0900

    adding login test

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./Register.module.css"
 
 const Register = () => {
+  const [userId, setUserId] = useState("");
+  const [pass,setPass] = useState("");
+
+  useEffect(()=>{
+    console.log("userId",userId)
+    console.log("pass",pass)
+  },[userId,pass])
+
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <h2 className={styles.loginTitle}>로그인</h2>
+      <input onChange={(event)=>setUserId(event.target.value)} placeholder='아이디 입력' className={styles.idInput}/>
+      <input onChange={(event)=>setPass(event.target.value)} placeholder='비밀번호 입력' className={styles.passInput}/>
+      <button onClick={}>로그인</button>
     </div>
   )
 }
