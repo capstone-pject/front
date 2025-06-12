beb40fe7292efc3da08a75914c60087155bca840
bmbx08 <bmbx08@bu.ac.kr>
2025-06-12 15:36:51 +0900

    added login, register css

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,15 +1,19 @@
 import React, {useEffect, useState} from "react";
 import "./Register.style.css";
 import useRegister from "../../hooks/user/useRegister";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
     // userInfoId: 0
+    userName:"",
     userId: "",
     password: "",
     birth: "",
   });
   const registerMutation = useRegister();
+
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const {name, value} = e.target;
@@ -22,7 +26,7 @@ const Register = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    registerMutation.mutate(formData);
+    registerMutation.mutate(formData,{onSuccess: () => navigate("/")});
   };
 
   useEffect(() => {
@@ -31,41 +35,49 @@ const Register = () => {
 
   return (
     <div className="register-page">
-      <form onSubmit={handleSubmit} className="register-box">
-        <div className="register-title">회원가입</div>
+      <div className="register-title">약(藥)속해요 회원가입</div>
+      <form onSubmit={handleSubmit} className="flex-column">
+        <div className="register-box">
+          <div className="register-row-name">이름</div>
+          <input
+            name="userName"
+            onChange={handleChange}
+            className="register-row-input"
+            placeholder="이름을 작성해주세요."
+            required
+          />
 
-        <div className="register-row">
           <div className="register-row-name">아이디</div>
           <input
             name="userId"
             onChange={handleChange}
-            className="register-input"
+            className="register-row-input"
+            placeholder="아이디를 작성해주세요."
             required
           />
-        </div>
 
-        <div className="register-row">
           <div className="register-row-name">비밀번호</div>
           <input
             type="password"
             name="password"
             onChange={handleChange}
-            className="register-input"
+            className="register-row-input"
+            placeholder="아이디를 작성해주세요."
+            required
           />
-        </div>
 
-        <div className="register-row">
           <div className="register-row-name">생일</div>
           <input
             type="date"
             onChange={handleChange}
             name="birth"
-            className="register-input"
+            className="register-row-input"
             required
           />
-        </div>
-        <button type="submit">회원가입</button>
+
         {/* <button onClick={registerUser}>회원가입 테스트</button> */}
+        </div>
+        <button type="submit" className="register-button">회원가입</button>
       </form>
     </div>
   );
